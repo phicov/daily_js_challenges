55ce427a823a0b8c54021a339d1a82878a856c7d
Leon Glaser <leon.glaser@gmail.com>
2022-08-24 21:59:03 -0400

    challenge 15 completed

--- challenges.js
+++ challenges.js
@@ -406,7 +406,10 @@ mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  //=> {a: 1, b: 2, c: 3, d: 4}
 mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  //=> {a: 1, b: 22, c: 3, d: 44}
 -----------------------------------------------------------------*/
 // Your solution for 15-mergeObjects here:
-function mergeObjects(obj1, obj2) {}
+function mergeObjects(obj1, obj2, obj3) {
+  let resultObject = Object.assign(obj1, obj2, obj3);
+  return resultObject;
+}
 /*-----------------------------------------------------------------
 Challenge: 16-findHighestPriced
 
--- console.js
+++ console.js
@@ -26,3 +26,37 @@ function fromPairs(arr) {
 }
 fromPairs();
 //use Object.fromEntries{} ??
+// -----------------------------------------------------------------*/
+// Challenge: 15-mergeObjects
+
+// Difficulty:  Intermediate
+
+// Prompt:
+
+// - Write a function named mergeObjects that accepts at least two objects as arguments, merges the properties of the second through n objects into the first object, then finally returns the first object.
+// - If any objects have the same property key, values from the object(s) later in the arguments list should overwrite earlier values.
+
+// Examples:
+
+// mergeObjects({}, {a: 1});  //=> {a: 1} (same object as first arg)
+// mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  //=> {a: 1, b: 2, c: 3, d: 4}
+// mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  //=> {a: 1, b: 22, c: 3, d: 44}
+// -----------------------------------------------------------------*/
+// Your solution for 15-mergeObjects here:
+function mergeObjects(obj1, obj2) {
+  obj1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    ssn: "123-456-2356",
+    jobTitle: "Fart sniffer",
+  };
+
+  obj2 = {
+    jobTitle: "JavaScript Developer",
+    country: "USA",
+  };
+  let resultObject = Object.assign(obj1, obj2);
+  console.log(resultObject);
+}
+mergeObjects();
